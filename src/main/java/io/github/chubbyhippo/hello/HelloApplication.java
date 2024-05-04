20212f91d16dac62df316d89f6be1b2eaa8d4f49
ch <mkmsacc@outlook.com>
2024-05-04 23:00:42 +0700

    Add HelloController and update corresponding tests
    Added a new rest controller named HelloController with log and endpoint for "/hello". Updated test suites to accommodate the new controller, including the injection of the HelloController and adding mock server tests. Also, added service.url property in the application properties.

--- src/main/java/io/github/chubbyhippo/hello/HelloApplication.java
+++ src/main/java/io/github/chubbyhippo/hello/HelloApplication.java
@@ -1,13 +1,50 @@
 package io.github.chubbyhippo.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
 
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+}
+
+@RestController
+class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @Value("${service.url}")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    private final RestTemplate restTemplate;
+
+    HelloController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/hello")
+    String getHello() {
+        log.info("Service URL: {}", url);
+        return restTemplate.getForObject(url, String.class);
+    }
 }
--- src/test/java/io/github/chubbyhippo/hello/HelloApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/hello/HelloApplicationTests.java
@@ -1,13 +1,43 @@
 package io.github.chubbyhippo.hello;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WireMockTest
 class HelloApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @RegisterExtension
+    private final static WireMockExtension wm = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort()
+                    .dynamicHttpsPort())
+            .build();
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @DisplayName("should get hello using setter injection")
+    void shouldGetHelloUsingSetterInjection(@Autowired HelloController controller) {
+        wm.stubFor(get("/hello").willReturn(aResponse().withBody("hello")));
+        controller.setUrl(wm.getRuntimeInfo().getHttpBaseUrl() + "/hello");
+
+        webTestClient.get()
+                .uri("/hello")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("hello");
+    }
 }
