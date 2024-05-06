3b42cb54a8f3073389adfc61c635241bab4107a0
ch <mkmsacc@outlook.com>
2024-05-06 22:20:47 +0700

    Remove unused imports and HelloController class
    The changes include the removal of unused imports, specifically Logger, LoggerFactory, value, GetMapping, and RestController from the HelloApplication file. Furthermore, the HelloController class was completely removed as it was no longer needed in this file.

--- src/main/java/io/github/chubbyhippo/hello/HelloApplication.java
+++ src/main/java/io/github/chubbyhippo/hello/HelloApplication.java
@@ -1,14 +1,9 @@
 package io.github.chubbyhippo.hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -25,26 +20,3 @@ public class HelloApplication {
 
 }
 
-@RestController
-class HelloController {
-    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
-
-    @Value("${service.url}")
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    private String url;
-
-    private final RestTemplate restTemplate;
-
-    HelloController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    @GetMapping("/hello")
-    String getHello() {
-        log.info("Service URL: {}", url);
-        return restTemplate.getForObject(url, String.class);
-    }
-}
